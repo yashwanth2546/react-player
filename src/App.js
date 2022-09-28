7d7b70ca679826e0f06512dc7803b27b9ecbac9d
yashwant <yashwant.k.me@gmail.com>
2022-09-28 18:13:58 +0530

    --done basic player with controls

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import { Player, LoadingSpinner, BigPlayButton, ControlBar, ReplayControl, ForwardControl, PlaybackRateMenuButton } from 'video-react';
+
+import 'video-react/dist/video-react.css'; // import css
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+          <LoadingSpinner />
+          <BigPlayButton position="center" />
+          <ControlBar>
+              <PlaybackRateMenuButton rates={[6, 2, 1.5, 1, 0.5, 0.1]}/>
+              <ReplayControl seconds={10} />
+              <ForwardControl seconds={10} />
+          </ControlBar>
+      </Player>
   );
 }
 
