0907e08e1e72c116677b344e623318edc71c2b0d
KARTHIKEYA <Surak504@gmail.com>
2022-09-28 22:32:31 +0530

    --added feature closed captions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Player, LoadingSpinner, BigPlayButton, ControlBar, ReplayControl, ForwardControl, PlaybackRateMenuButton } from 'video-react';
+import { Player, LoadingSpinner, BigPlayButton, ControlBar, ReplayControl, ForwardControl, PlaybackRateMenuButton, ClosedCaptionButton } from 'video-react';
 
 import 'video-react/dist/video-react.css'; // import css
 
 function App() {
   return (
-      <Player src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
+      <Player>
+          <source src="../public/assets/sample.mp4"
+                  type="video/mp4"
+          />
+          <track kind="cations"
+                 src="../public/assets/sample.en.vtt"
+                 srcLang="en"
+                 label="English"
+                 default
+          />
           <LoadingSpinner />
           <BigPlayButton position="center" />
           <ControlBar>
               <PlaybackRateMenuButton rates={[6, 2, 1.5, 1, 0.5, 0.1]}/>
               <ForwardControl seconds={10} />
               <ReplayControl seconds={10} />
+              <ClosedCaptionButton />
           </ControlBar>
       </Player>
   );
